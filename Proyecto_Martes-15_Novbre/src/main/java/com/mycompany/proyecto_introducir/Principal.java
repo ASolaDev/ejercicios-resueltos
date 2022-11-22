6ad62000f7594ab715eac08079274a3cd4c0c908
IsabelPuertas9 <i.puertas.planes@gmail.com>
2022-11-22 19:53:50 +0100

    Solucion a la tarea 4-1 del Tema4

--- Proyecto_Martes-15_Novbre/src/main/java/com/mycompany/proyecto_introducir/Principal.java
+++ Proyecto_Martes-15_Novbre/src/main/java/com/mycompany/proyecto_introducir/Principal.java
@@ -13,8 +13,9 @@ public class Principal {
 
     public static void main(String[] args) {
         
-      
-        int tren[]=new int[5];
+      /*
+        int tren[];
+        tren=new int[5];
         
         //El array tren se acaba de crear, digamos que es nuevo, por tanto, sus vagones están vacíos, con 0 personas
         for (int i = 0; i < 5; i++) {
@@ -50,7 +51,7 @@ public class Principal {
         for (int i = 0; i < v.length; i++) { //Para introducir enteros por teclado, utilizo la clase Introducir, método: entero
             /* int n=Introducir.entero("Dime el valor de v["+i+"]");            
                v[i]=n;
-            */
+            
             v[i]=Introducir.entero("Dime el valor de v["+i+"]");            
         }
         
@@ -58,6 +59,70 @@ public class Principal {
         for (int i = 0; i < v.length; i++) {
             System.out.println("v["+i+"]="+v[i]);
         }     
+        
+        final String meses[]={"","Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septbre","Octbre","Novbre","Dcbre"};
+        int dias[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+        
+        int dia=Introducir.enteroPositivo("Dime un día: ");
+        int mes=Introducir.enteroPositivo("Dime un mes: ");
+        int anio=Introducir.enteroPositivo("Dime un año: ");
+        
+        int sumaDias=0;
+        if (anio%4==0) dias[2]=29; 
+        for (int i = 1; i < mes ; i++) {
+            sumaDias=sumaDias + dias[i];
+        }
+        sumaDias+=dia;
+        
+        
+       //meses[7]="José";
+       
+       */
+       int w[]=new int[4];// w=023fa   0 0 0 0
+       w[0]=89; //023fa 
+       w[1]=89;
+       w[2]=89;
+       w[3]=89;
+       
+       int x[]=new int[4];// x=1234 // 0 0 0 0
+       
+       x[2]=w[3]; // x 0 0 89 0
+       x[2]++; //x 0 0 90 0
+       
+       
+       int y=9, z=7;
+       y=z;// y=7
+       
+       
+       x=w;// x  89 89 89 89 // x toma la misma dirección de memoria que guarda w
+       
+       for (int i = 0; i < w.length; i++) {
+            System.out.println(w[i]);
+        }
+        
+       
+       x[2]++; // 89 89 90 89
+       for (int i = 0; i < w.length; i++) {
+            System.out.println(w[i]);
+        }
+       
+       
+       /* Introducir 5 números enteros y calcular cuál es el mayor*/
+       int m[]=new int[5];
+       
+        for (int i = 0; i < 5; i++) {
+            m[i]=Introducir.entero("Dime un número: ");
+        }
+        
+        int my=Integer.MIN_VALUE;
+        for (int i = 0; i < 5; i++) {
+            if (m[i]>my) my=m[i];
+        }
+        
+        System.out.println("El mayor de todos es "+my);
+        
+       
+        
         
     }
 }
---
+++ Tarea4_1/src/main/java/com/mycompany/tarea4_1/Ejercicio1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tarea4_1;
+
+/**
+ *
+ * @author isabel
+ */
+
+/*
+    Mostrad los nombres de aquellos meses que tengas días pares
+*/
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+       String meses[]={"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septbre","Octbre","Novbre","Dcbre"};
+       int dias[]={31,28,31,30,31,30,31,31,30,31,30,31};
+       
+        System.out.println("Los meses que tienen días par son: ");
+        for (int i = 0; i < meses.length; i++) {
+            if (dias[i]%2 == 0) //el número de días es par?
+                System.out.println(meses[i]);
+        }
+       
+    }
+}
