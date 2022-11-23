96b6a8c1f7486721c2b151caa652399eda2a1d9a
IsabelPuertas9 <i.puertas.planes@gmail.com>
2022-11-23 18:57:48 +0100

    Métodos con Arrays - Proyecto: MetodosArrays

---
+++ MetodosArrays/src/main/java/com/mycompany/metodosarrays/Principal1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.metodosarrays;
+
+/**
+ *
+ * @author isabel
+ */
+public class Principal1 {
+
+    public static void main(String[] args) {
+       int v[]={12,4,5,37,45,67,44,31,77,78};
+       
+       Vectores.mostrar_array_unidimensional_entero(v);
+    }
+}
---
+++ MetodosArrays/src/main/java/com/mycompany/metodosarrays/Principal3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.metodosarrays;
+
+/**
+ *
+ * @author isabel
+ */
+public class Principal3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int v[]={12,4,5,37,45,67,44,31,77,78};
+        
+        /*Ejemplo: Se pondrán todos los elementos del array con valor 20*/
+        Vectores.fill_array_unidimensional_entero(v, 20);
+        Vectores.mostrar_array_unidimensional_entero(v);
+    }
+    
+}
